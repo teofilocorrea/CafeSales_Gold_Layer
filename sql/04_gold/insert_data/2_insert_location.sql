873b840ebbed936ad946cec12326aefe1571aa5c
tcorrea <teofilocorrea@gmail.com>
2026-08-18 09:41:57 -0400

    feat: cargar datos en gold.dim_location desde silver.sales

---
+++ sql/04_gold/insert_data/2_insert_location.sql
@@ -0,0 +1,16 @@
+-- ============================================================
+-- Script : ETL — Silver → Gold | dim_location
+-- Tabla  : gold.dim_location
+-- Origen : silver.sales
+-- Autor  : Teofilo Correa Rojas
+-- Fecha  : 18 de agosto 2026
+-- ============================================================
+
+-- Ensayo — verificar el resultado antes de insertar
+SELECT DISTINCT COALESCE(location, 'Unknown')
+FROM silver.sales;
+
+-- ETL — cargar los productos únicos en la dimensión
+INSERT INTO gold.dim_location (location_name)
+SELECT DISTINCT COALESCE(location, 'Unknown')
+FROM silver.sales;
